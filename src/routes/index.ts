9afdf8a29207341ede511ae857886d90546810c5
vamsipe <vamsipe.in@mouritech.com>
2023-01-05 11:40:31 +0530

    deleted route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import account from "../routes/account";
-import userRegister from "../routes/userRegister";
 const router = express.Router();
 
 router.use("/restro", account);
